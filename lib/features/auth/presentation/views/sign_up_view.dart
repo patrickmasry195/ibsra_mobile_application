0849ac72b063d5fad4e3bc537fb1ea9f136d7a92
ahmedelhalabi4 <elhalabiahmed4@gmail.com>
2025-08-03 15:08:13 +0300

    fix some problems

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-<<<<<<< HEAD
-=======
-import 'package:ibsra_mobile_application/core/widgets/primary_button.dart';
-
->>>>>>> 4384a3b (update: add login design)
 import '../../../../core/utils/styles.dart';
 import '../../../../generated/assets.dart';
 import '../widgets/auth_social_button.dart';
 import '../widgets/auth_text_form_field.dart';
-import 'login_view.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
@@ -17,7 +11,6 @@ class SignUpView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
@@ -92,70 +85,6 @@ class SignUpView extends StatelessWidget {
                 ),
               ),
             ),
-=======
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Register',
-                style: Styles.specialTitle30,
-              ),
-              SizedBox(height: 44.0),
-              SvgPicture.asset(Assets.imagesLockIcon),
-              SizedBox(height: 48.0),
-              AuthTextFormField(hintText: 'Email'),
-              SizedBox(height: 13.0),
-              AuthTextFormField(hintText: 'Password'),
-              SizedBox(height: 13.0),
-              AuthTextFormField(hintText: 'Confirm Password'),
-              SizedBox(height: 13.0),
-              PrimaryButton(text: 'Registrasi'),
-              SizedBox(height: 33.0),
-              Text(
-                'Atau login dengan',
-                style: Styles.labelStyle14,
-              ),
-              SizedBox(height: 32.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  AuthSocialButton(
-                    onPressed: () {},
-                    icon: Assets.imagesGoogleLogo,
-                  ),
-                  SizedBox(width: 21.0),
-                  AuthSocialButton(
-                    onPressed: () {},
-                    icon: Assets.imagesPhone,
-                  ),
-                ],
-              ),
-              SizedBox(height: 33.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Sudah punya akun? ',
-                    style: Styles.linkStyle12,
-                  ),
-                  GestureDetector(
-                    onTap: () => Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (_) => LoginView(),
-                      ),
-                    ),
-                    child: Text(
-                      'Login',
-                      style: Styles.linkStyle12.copyWith(color: Colors.blue),
-                    ),
-                  ),
-                ],
-              ),
-            ],
->>>>>>> 4384a3b (update: add login design)
           ),
         ],
       ),
